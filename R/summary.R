a10a8c0c092178db314944f15e0f21282439452f
Pascal Croteau <croteaupascl@gmail.com>
2026-08-09 12:19:16 -0400

    summary now reports the alpha inference

--- R/summary.R
+++ R/summary.R
@@ -3,6 +3,9 @@ summary.geecor <- function(object, ...)
 {
   value <- geepack:::summary.geeglm(object)
   value$corstr <- object$.corstruct
+  value$corr <- value$geese$correlation
+  colnames(value$corr) <- c("Estimate", "san.se", "Wald", "Pr(>|W|)")
+
   # value$call <- object$callorig
   value
 }
